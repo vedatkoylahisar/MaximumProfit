4361b778d6405a75c4c9cd1bf645026555d74bf9
vedatkoylahisar <vedatkylhsr@gmail.com>
2024-07-26 09:34:29 +0300

    Düzenlemeler yapıldı

--- Program.cs
+++ Program.cs
@@ -1,96 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.Office.Interop.Excel;
-using Excel = Microsoft.Office.Interop.Excel;
 
-public class Trade
+public class Model
 {
-    public static void Main(string[] args)
+    public double AlisFiyati { get; set; }
+    public double SatisFiyati { get; set; }
+    public DateTime Tarih { get; set; }
+}
+
+class Program
+{
+    static void Main()
     {
-
-        Excel.Application xlApp = new Excel.Application();
-        string filePath = @"C:\Users\vedat\source\repos\Trade\1 Aylık Döviz Alış ve Satış Dokümanı.xlsx";
-        Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(filePath);
-        Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
-        Excel.Range xlRange = xlWorksheet.UsedRange;
-
-        int maxRow = xlRange.Rows.Count;
-        int maxColumn = xlRange.Columns.Count;
-
-        string alis = "Alış";
-        string satis = "Satış";
-
-        int AlisColumn = -1;
-        int SatisColumn = -1;
-
-        for (int col = 1; col <= maxColumn; col++)
+        List<Model> dovizKurlari = new List<Model>()
         {
-            if ((xlRange.Cells[1, col] as Excel.Range).Value2 != null)
+            new Model { AlisFiyati = 13.58, SatisFiyati = 13.61, Tarih = new DateTime(2024, 5, 18) },
+            new Model { AlisFiyati = 13.61, SatisFiyati = 13.67, Tarih = new DateTime(2024, 5, 19) },
+            new Model { AlisFiyati = 26.64, SatisFiyati = 26.90, Tarih = new DateTime(2024, 5, 20) },
+            new Model { AlisFiyati = 10.00, SatisFiyati = 10.57, Tarih = new DateTime(2024, 5, 21) },
+            new Model { AlisFiyati = 11.57, SatisFiyati = 11.70, Tarih = new DateTime(2024, 5, 22) },
+            new Model { AlisFiyati = 11.69, SatisFiyati = 11.74, Tarih = new DateTime(2024, 5, 23) },
+            new Model { AlisFiyati = 11.80, SatisFiyati = 11.87, Tarih = new DateTime(2024, 5, 24) },
+            new Model { AlisFiyati = 11.74, SatisFiyati = 11.88, Tarih = new DateTime(2024, 5, 25) },
+            new Model { AlisFiyati = 11.77, SatisFiyati = 11.89, Tarih = new DateTime(2024, 5, 26) },
+            new Model { AlisFiyati = 11.88, SatisFiyati = 12.00, Tarih = new DateTime(2024, 5, 27) },
+            new Model { AlisFiyati = 12.80, SatisFiyati = 12.55, Tarih = new DateTime(2024, 5, 28) },
+            new Model { AlisFiyati = 13.80, SatisFiyati = 13.00, Tarih = new DateTime(2024, 5, 29) },
+            new Model { AlisFiyati = 14.00, SatisFiyati = 13.89, Tarih = new DateTime(2024, 5, 30) },
+            new Model { AlisFiyati = 13.57, SatisFiyati = 13.59, Tarih = new DateTime(2024, 5, 31) },
+            new Model { AlisFiyati = 13.60, SatisFiyati = 13.99, Tarih = new DateTime(2024, 6, 1) },
+            new Model { AlisFiyati = 13.75, SatisFiyati = 14.00, Tarih = new DateTime(2024, 6, 2) },
+            new Model { AlisFiyati = 13.67, SatisFiyati = 13.89, Tarih = new DateTime(2024, 6, 3) },
+            new Model { AlisFiyati = 13.77, SatisFiyati = 13.79, Tarih = new DateTime(2024, 6, 4) },
+            new Model { AlisFiyati = 14.00, SatisFiyati = 13.99, Tarih = new DateTime(2024, 6, 5) },
+            new Model { AlisFiyati = 14.25, SatisFiyati = 14.33, Tarih = new DateTime(2024, 6, 6) },
+            new Model { AlisFiyati = 14.30, SatisFiyati = 14.40, Tarih = new DateTime(2024, 6, 7) },
+            new Model { AlisFiyati = 14.85, SatisFiyati = 15.00, Tarih = new DateTime(2024, 6, 8) },
+            new Model { AlisFiyati = 15.07, SatisFiyati = 15.74, Tarih = new DateTime(2024, 6, 9) },
+            new Model { AlisFiyati = 15.25, SatisFiyati = 15.47, Tarih = new DateTime(2024, 6, 10) },
+            new Model { AlisFiyati = 14.87, SatisFiyati = 15.07, Tarih = new DateTime(2024, 6, 11) },
+            new Model { AlisFiyati = 14.89, SatisFiyati = 14.97, Tarih = new DateTime(2024, 6, 12) },
+            new Model { AlisFiyati = 15.08, SatisFiyati = 15.15, Tarih = new DateTime(2024, 6, 13) },
+            new Model { AlisFiyati = 15.50, SatisFiyati = 15.65, Tarih = new DateTime(2024, 6, 14) },
+            new Model { AlisFiyati = 19.25, SatisFiyati = 19.50, Tarih = new DateTime(2024, 6, 15) },
+            new Model { AlisFiyati = 18.25, SatisFiyati = 18.34, Tarih = new DateTime(2024, 6, 16) }
+        };
+
+        var hesapla = new Hesapla(dovizKurlari);
+        hesapla.MaxKarHesapla();
+    }
+}
+
+class Hesapla
+{
+    private List<Model> _dovizKurlari;
+
+    public Hesapla(List<Model> dovizKurlari)
+    {
+        _dovizKurlari = dovizKurlari;
+    }
+
+    public void MaxKarHesapla()
+    {
+        int n = _dovizKurlari.Count;
+        if (n == 0) return;
+
+        double minAlisFiyati = _dovizKurlari[0].AlisFiyati;
+        DateTime minAlisTarihi = _dovizKurlari[0].Tarih;
+        double maxKar = 0;
+        DateTime alisTarihi = _dovizKurlari[0].Tarih;
+        DateTime satisTarihi = _dovizKurlari[0].Tarih;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (_dovizKurlari[i].AlisFiyati < minAlisFiyati)
             {
-                if ((xlRange.Cells[1, col] as Excel.Range).Value2.ToString().Contains(alis))
-                {
-                    AlisColumn = col;
-                }
-                if ((xlRange.Cells[1, col] as Excel.Range).Value2.ToString().Contains(satis))
-                {
-                    SatisColumn = col;
-                }
+                minAlisFiyati = _dovizKurlari[i].AlisFiyati;
+                minAlisTarihi = _dovizKurlari[i].Tarih;
+            }
+
+            double kar = _dovizKurlari[i].SatisFiyati - minAlisFiyati;
+            if (kar > maxKar)
+            {
+                maxKar = kar;
+                alisTarihi = minAlisTarihi;
+                satisTarihi = _dovizKurlari[i].Tarih;
             }
         }
 
-        List<(DateTime date, double rate)> alisList = new List<(DateTime, double)>();
-        List<(DateTime date, double rate)> satisList = new List<(DateTime, double)>();
-
-
-        for (int row = 2; row <= maxRow; row++)
-        {
-            DateTime date = DateTime.FromOADate((xlRange.Cells[row, 1] as Excel.Range).Value2);
-
-            var alisValue = (xlRange.Cells[row, AlisColumn] as Excel.Range).Value2;
-            var satisValue = (xlRange.Cells[row, SatisColumn] as Excel.Range).Value2;
-
-            if (alisValue != null)
-            {
-                alisList.Add((date, alisValue));
-            }
-
-            if (satisValue != null)
-            {
-                satisList.Add((date, satisValue));
-            }
-        }
-
-        alisList.Sort((a, b) => a.date.CompareTo(b.date));
-        satisList.Sort((a, b) => a.date.CompareTo(b.date));
-
-        double maxProfit = 0;
-        DateTime bestBuyDate = DateTime.MinValue;
-        DateTime bestSellDate = DateTime.MinValue;
-        int alisCount = alisList.Count;
-        int satisCount = satisList.Count;
-
-        for (int i = 0; i < alisCount; i++)
-        {
-            for (int j = i + 1; j < satisCount; j++)
-            {
-                if (satisList[j].date > alisList[i].date)
-                {
-                    double profit = satisList[j].rate - alisList[i].rate;
-                    if (profit > maxProfit)
-                    {
-                        maxProfit = profit;
-                        bestBuyDate = alisList[i].date;
-                        bestSellDate = satisList[j].date;
-                    }
-                }
-            }
-        }
-
-        Console.WriteLine($"Best day to buy: {bestBuyDate.ToShortDateString()} at rate {alisList.Find(a => a.date == bestBuyDate).rate}");
-        Console.WriteLine($"Best day to sell: {bestSellDate.ToShortDateString()} at rate {satisList.Find(s => s.date == bestSellDate).rate}");
-        Console.WriteLine($"Maximum profit: {maxProfit}");  
-        Console.WriteLine("Time Complexity: O(n^2)");
-
-
+        Console.WriteLine("Max kar: " + maxKar);
+        Console.WriteLine("Alis tarihi: " + alisTarihi.ToString("yyyy-MM-dd"));
+        Console.WriteLine("Satis tarihi: " + satisTarihi.ToString("yyyy-MM-dd"));
     }
 }
